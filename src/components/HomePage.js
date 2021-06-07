18025b23dd7ce3c129d17da8fbb159ccfc5480ae
kslewis19 <60162532+kslewis19@users.noreply.github.com>
2021-06-07 14:19:28 -0400

    added test logout button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,28 @@
+import { UserContext } from "../contexts/UserContext";
+import { useContext } from "react";
+import firebase from "../firebase/firebase";
+import { Button } from "@material-ui/core";
 
+export default function HomePage() {
+    const { firstName, isLoggedIn } = useContext(UserContext);
+    if (isLoggedIn) {
+        return (<div>
+            hello {firstName}
+            <br />
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                style={{ width: "12em", backgroundColor: "#2E3B55" }}
+                onClick={() => {
+                    firebase.auth().signOut();
+                    window.location.reload();//refresh page
+                }}
+            >
+                sign out
+        </Button>
+        </div>)
+    }
 
-export default function HomePAge() {
-
-    return (<div> this is the home page</div>)
+    return (<div> Hello! you are not logged in</div>)
 }
